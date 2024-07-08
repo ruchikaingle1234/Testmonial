baeb9c36f586a288fa23fa082dd8525bed894ca0
Ruchika <ruchikaringle9@gmail.com>
2024-07-08 20:14:06 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import CarouselComponent from './Component/CarouselComponent';
+
+function App() {
+ 
+  return(
+    
+    <CarouselComponent/>
+  )
+}
+
+export default App;
